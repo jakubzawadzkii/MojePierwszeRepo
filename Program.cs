cae2dbbb131d22947d393d6aeb01cbe6492aefad
jakubzawadzkii <chx101558@student.chorzow.merito.pl>
2025-10-20 20:57:50 +0200

    Fixed code

--- Program.cs
+++ Program.cs
@@ -70,7 +70,7 @@ class Program
 
 
 
-                        if (count /*??*/ 0)
+                        if (count == 0)
 
                         {
 
@@ -126,7 +126,7 @@ class Program
 
         char[,] board = new char[height, width];
 
-        Random random = /*??*/
+        Random random = new Random();
 
 
 
@@ -189,7 +189,7 @@ class Program
 
 
         /*??*/
-        displayBoard;
+        return displayBoard;
 
     }
 
@@ -261,7 +261,7 @@ class Program
 
         {
 
-            for (int j = Math.Max(0, x - 1); j <= /*??*/(width - 1, x + 1); j++)
+            for (int j = Math.Max(0, x - 1); j <= Math.Min(width - 1, x + 1); j++)
 
             {
 
@@ -363,7 +363,7 @@ class Program
 
 
 
-        return uncoveredCount == /* ??*/ *height - bombCount;
+        return uncoveredCount == width * height - bombCount;
 
     }
 
